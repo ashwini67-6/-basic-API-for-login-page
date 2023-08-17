23047d5666cf796db4d74d047372fac79e230957
Ashwini <115686030+ashwini67-6@users.noreply.github.com>
2023-08-17 18:38:10 +0530

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,28 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app =express();
+const port = 3000;
+var idname ="";
+
+app.use(bodyParser.urlencoded({extended:true}));
+function idnameGenerator(req,res,next){
+    console.log(req.body);
+    idname= req.body[" uname"] +req.body["psw"];
+    next();
+}
+ app.use(idnameGenerator);
+
+ app.get("/",(req,res) =>{
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit",(req,res) =>{
+    res.send("Your credentials are saved...!!!");
+});
+
+app.listen(port,() =>{
+    console.log("Server is running on ${port}");
+});
